e916825de61c5720cc1a0d3a48d60f1d756b458b
Ariel Squatrito <squatritoariel@gmail.com>
2023-07-23 14:21:07 -0300

    lista de tareas ok

--- lista-de-tareas/app.js
+++ lista-de-tareas/app.js
@@ -17,9 +17,11 @@ function agregarTarea() {
 
     let completar = document.createElement("i");
     completar.classList.add("bi", "bi-check-circle-fill", "icono-completar");
+    completar.addEventListener("click", completarTarea);
 
     let eliminar = document.createElement("i");
     eliminar.classList.add("bi", "bi-trash3-fill", "icono-eliminar");
+    eliminar.addEventListener("click", eliminarTarea);
 
     iconos.append(completar, eliminar);
 
@@ -29,4 +31,20 @@ function agregarTarea() {
   }
 }
 
+function completarTarea(e) {
+  let tarea = e.target.parentNode.parentNode;
+  tarea.classList.toggle("completada");
+}
+
+function eliminarTarea(e) {
+  let tarea = e.target.parentNode.parentNode;
+  tarea.remove();
+}
+
 boton.addEventListener("click", agregarTarea);
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    agregarTarea();
+  }
+});
